2b1819dc70f0fee9d8a43d40c27c5518df9bd650
cameron87423 <87423@farnborough.ac.uk>
2019-07-04 16:11:24 +0100

    Test

--- src/main/java/InformationController.java
+++ src/main/java/InformationController.java
@@ -2,7 +2,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class InformationController {
-    public static void Iselect() {
+    public static void Iselect() {   //test
         try {
             PreparedStatement ps = Main.db.prepareStatement("SELECT * FROM Information");
             ResultSet results = ps.executeQuery();
