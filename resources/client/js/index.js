6b3a842e41635fc825c2669a4abcf3d9662a3edd
cameron87423 <87423@farnborough.ac.uk>
2019-11-28 16:08:45 +0000

    change the exception inside the try statements and look for git bash for testing it

--- resources/client/js/index.js
+++ resources/client/js/index.js
@@ -1,13 +1,6 @@
 function pageLoad() {
-
-    let now = new Date();
-    let myHTML = '<div style="text-align:center;">'
-        + '<h1>Welcome to my API powered website!</h1>'
-        + '<img src="/client/img/logo.jfif"  alt="Logo"/>'
-        + '<div style="font-style: italic;">'
-        + 'Generated at ' + now.toLocaleTimeString()
-        + '</div>'
-        + '</div>';
+    let myHTML = '<div style="align:left;"/>'
+        +'<img src="/client/img/logo.jfif"  alt="Logo"/>'
     document.getElementById("testDiv").innerHTML = myHTML;
 
 }
