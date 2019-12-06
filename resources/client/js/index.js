89906038fc9a023bfe578a15b28bd3289b93abd8
cameron87423 <87423@farnborough.ac.uk>
2019-12-06 18:04:44 +0000

    password stuff finished

--- resources/client/js/index.js
+++ resources/client/js/index.js
@@ -14,29 +14,81 @@ function login(event) {
     event.preventDefault();
     const form = document.getElementById("loginForm");
     const formData = new FormData(form);
-    fetch("/user/login", {method: 'post', body: formData}
+    if (document.getElementsByName("user").value = "student") {
+        fetch("/Students/login", {method: 'post', body: formData}
+        ).then(response => response.json()
+        ).then(responseData => {
+            if (responseData.hasOwnProperty('error')) {
+                alert(responseData.error);
+            } else {
+                Cookies.set("username", responseData.FName);
+                Cookies.set("token", responseData.token);
+                window.location.href = '/client/student.html';
+            }
+        });
+    }else if (document.getElementsByName("user").value = "tutor") {
+        fetch("/Tutors/login", {method: 'post', body: formData}
+        ).then(response => response.json()
+        ).then(responseData => {
+            if (responseData.hasOwnProperty('error')) {
+                alert(responseData.error);
+            } else {
+                Cookies.set("username", responseData.TFName);
+                Cookies.set("token", responseData.token);
+                window.location.href = '/client/tutor.html';
+            }
+        });
+    }else if (document.getElementsByName("user").value = "parent") {
+        fetch("/Parents/login", {method: 'post', body: formData}
+        ).then(response => response.json()
+        ).then(responseData => {
+            if (responseData.hasOwnProperty('error')) {
+                alert(responseData.error);
+            } else {
+                Cookies.set("username", responseData.PFName);
+                Cookies.set("token", responseData.token);
+                window.location.href = '/client/parent.html';
+            }
+        });
+    }
+}
+
+function logout() {
+    fetch("/Students/logout", {method: 'post'}
     ).then(response => response.json()
     ).then(responseData => {
         if (responseData.hasOwnProperty('error')) {
             alert(responseData.error);
         } else {
-            Cookies.set("username", responseData.username);
-            Cookies.set("token", responseData.token);
-            window.location.href = '/client/index.html';
+            Cookies.remove("FName");
+            Cookies.remove("token");
+            window.location.href = '/client/login.html';
         }
     });
 }
-function logout() {
-    fetch("/user/logout", {method: 'post'}
+function Plogout() {
+    fetch("/Parents/logout", {method: 'post'}
     ).then(response => response.json()
     ).then(responseData => {
         if (responseData.hasOwnProperty('error')) {
             alert(responseData.error);
         } else {
-            Cookies.remove("username");
+            Cookies.remove("PFName");
             Cookies.remove("token");
-            window.location.href = '/client/index.html';
+            window.location.href = '/client/login.html';
         }
     });
-
+}
+function Tlogout() {
+    fetch("/Tutors/logout", {method: 'post'}
+    ).then(response => response.json()
+    ).then(responseData => {
+        if (responseData.hasOwnProperty('error')) {
+            alert(responseData.error);
+        } else {
+            Cookies.remove("TFName");
+            Cookies.remove("token");
+            window.location.href = '/client/login.html';
+        }
+    });
 }
